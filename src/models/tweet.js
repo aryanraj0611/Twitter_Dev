5222e43de35ee135a5aa0037ae8b651915a40482
aryanraj0611 <aryanraj01010@gmail.com>
2026-04-16 19:39:57 +0530

    added hashtag model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -3,26 +3,17 @@ const mongoose = require('mongoose')
 const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
-        required: true
+        required: true,
+        max: [250, 'Tweet cannot be more than 250 characters']
     },
-    userEmail: {
-        type: String
-    },
-    comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment"
-    }]
+    hashtags: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Hashtag'
+        }
+    ]
 }, {timestamps: true})
 
-tweetSchema.virtual('contentWithEmail').get(function process(){
-    return this.content + "\n" + "created by: " + this.userEmail;
-})
-
-tweetSchema.pre('save', function(next){
-    console.log("inside a hook");
-    this.content = this.content + '....';
-    next();
-})
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
