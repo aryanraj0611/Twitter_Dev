a61ef0d22b7b66f11225fbf5e3c109816c7bd6b3
aryanraj0611 <aryanraj01010@gmail.com>
2026-04-18 19:54:10 +0530

    added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ const express = require('express')
 const app = express();
 const connect = require('./config/database')
 
-const Tweet = require('./models/tweet')
-
 
 app.listen(3000, async () => {
     console.log("Server started")
     await connect();
     console.log("MongoDB connected")
-    const tweets= await Tweet.find({
-        content: ["First tweet", "My tweet", "12m3 4"]
-        
-    })
-    console.log(tweets); 
-     
-})
+    let service = new TweetService();
+
+});
